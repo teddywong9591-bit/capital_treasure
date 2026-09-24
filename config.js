aeba197660f50e51fcb882c335f74bb713636241
teddywong9591-bit <teddywong9591@gmail.com>
2026-09-24 17:32:31 +0630

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+//For Stationary
+const CONFIG = {
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzSZA9QShdBMOsANbtNjOe-R3Sd65wYjXaQu3mMkb7ZFSVw00RQtjptY8OYXKokhqufog/exec"
+};
+
+//For Manpower
+const CONFIG1 = {
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxyamder-SOVDHoRSlSfsEnBYLppeRIvnopHSvJ2BeXPaeeI_6SwrsWpRgd_lFZ_eL9/exec"
+};
+
